2cb4fffcec5de8e2a27556fa99529334d9b79364
Hao Chen <hach@microsoft.com>
2016-07-22 17:47:48 -0700

    Used urlparse replace url

--- src/client/NodeJS/ms-rest/lib/filters/redirectFilter.js
+++ src/client/NodeJS/ms-rest/lib/filters/redirectFilter.js
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information. 
 
-var url = require('url');
+var parse = require('url-parse');
 /**
 * Creates a filter that handles server redirects for Http Statuscode 307.
 */
@@ -18,12 +18,12 @@ exports.create = function (maximumRetries) {
           currentRetries < maximumRetries)) {
         currentRetries++;
         
-        if (url.parse(response.headers.location).hostname) {
+        if (parse(response.headers.location).hostname) {
           resource.url = response.headers.location;
         } else {
-          var urlObject = url.parse(resource.url);
-          urlObject.pathname = response.headers.location;
-          resource.url = url.format(urlObject);
+          var urlObject = parse(resource.url, true);
+          urlObject.set('pathname', response.headers.location);
+          resource.url = urlObject.toString();
         }
         // POST request with Status code 303 should be converted into a 
         // redirected GET request if the redirect url is present in the location header
