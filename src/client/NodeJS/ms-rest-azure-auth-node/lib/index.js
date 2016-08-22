0a6aa0dc7ea3eebdaa20ef0bbe4e9fa760df508b
Hao Chen <hach@microsoft.com>
2016-08-22 16:44:04 -0700

    Created authentication library for Azure NodeJS client runtime.

---
+++ src/client/NodeJS/ms-rest-azure-auth-node/lib/index.js
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+'use strict';
+
+exports.UserTokenCredentials = require('./credentials/userTokenCredentials');
+exports.ApplicationTokenCredentials = require('./credentials/applicationTokenCredentials');
+exports.DeviceTokenCredentials = require('./credentials/deviceTokenCredentials');
+exports.AzureEnvironment = require('./azureEnvironment');
+// Expose TokenCredentials or not?
+exports.TokenCredentials = require('ms-rest').TokenCredentials;
+exports.interactiveLogin = require('./login').interactive;
+exports.loginWithUsernamePassword = require('./login').withUsernamePassword;
+exports.loginWithServicePrincipalSecret = require('./login').withServicePrincipalSecret;
+
+exports = module.exports;
