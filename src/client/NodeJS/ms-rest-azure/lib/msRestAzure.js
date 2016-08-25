01aa1db4ce4da5be849a33d83eb65a2fa75577fc
Hao Chen <hach@microsoft.com>
2016-08-25 11:21:53 -0700

    [Interface changed]Refactor ms-rest-azure.  * cred, env and login are included in ms-rest-azure-auth-node  * ms-rest-azure has LRO, Error and azureServiceClient  * Test for ms-rest-azure failed thou

--- src/client/NodeJS/ms-rest-azure/lib/msRestAzure.js
+++ src/client/NodeJS/ms-rest-azure/lib/msRestAzure.js
@@ -2,21 +2,10 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 'use strict';
 
-var azureAuth = require('ms-rest-azure-auth-node');
-
 exports.AzureServiceClient = require('./azureServiceClient');
 exports.BaseResource = require('./baseResource');
 exports.CloudError = require('./cloudError');
 exports.TokenCredentials = require('ms-rest').TokenCredentials;
-
 exports.generateUuid = require('./utils').generateUuid;
-
-exports.UserTokenCredentials = azureAuth.UserTokenCredentials;
-exports.ApplicationTokenCredentials = azureAuth.ApplicationTokenCredentials;
-exports.DeviceTokenCredentials = azureAuth.DeviceTokenCredentials;
-exports.AzureEnvironment = azureAuth.AzureEnvironment;
-exports.interactiveLogin = azureAuth.interactiveLogin;
-exports.loginWithUsernamePassword = azureAuth.loginWithUsernamePassword;
-exports.loginWithServicePrincipalSecret = azureAuth.loginWithServicePrincipalSecret;
 
 exports = module.exports;
